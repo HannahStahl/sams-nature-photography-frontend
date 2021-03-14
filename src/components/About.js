4b1a3ba9c7fd1b108b6fef077a3a5c9f3bb07445
Hannah Stahl <hls62@cornell.edu>
2021-03-14 17:43:09 -0500

    Added image placeholder to About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import React from 'react';
 import content from '../content.json';
 
 const About = () => (
-  <div>
+  <div className="about">
+    <div className="img-placeholder" />
     <div className="bio">
       {content.bio.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
     </div>
