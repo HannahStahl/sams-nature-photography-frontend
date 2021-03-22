be343c503852cb5341f3b090f65e1f90da83ab8d
Hannah Stahl <hls62@cornell.edu>
2021-03-21 19:22:31 -0500

    Fade out modal on exit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -48,6 +48,20 @@ const Gallery = ({ match: { params: { gallerySlug } } }) => {
     width = height * imageWidthToHeightRatio;
   }
 
+  const closeModal = () => {
+    const enlargedPhoto = document.getElementById("enlarged-photo-with-details");
+    const leftArrow = document.getElementById("previous");
+    const rightArrow = document.getElementById("next");
+    const exitIcon = document.getElementById("exit");
+    const modalBackdrop = document.getElementsByClassName("modal-backdrop")[0];
+    enlargedPhoto.classList.add('fade-out');
+    leftArrow.classList.add('fade-out');
+    rightArrow.classList.add('fade-out');
+    exitIcon.classList.add('fade-out');
+    modalBackdrop.classList.add('fade-out');
+    setTimeout(() => setModalOpen(false), 250);
+  };
+
   return (
     <div>
       <h2>{gallery.name}</h2>
@@ -77,9 +91,9 @@ const Gallery = ({ match: { params: { gallerySlug } } }) => {
             animation={false}
             dialogAs={(props) => <EnlargedPhoto style={{ width, height }} {...props} />}
             show={modalOpen}
-            onHide={() => setModalOpen(false)}
+            onHide={closeModal}
           >
-            <div className="enlarged-photo-with-details">
+            <div id="enlarged-photo-with-details" className="enlarged-photo-with-details">
               <img className="enlarged-photo" src={url} alt={title} />
               <div className="photo-details">
                 <h4>{title}</h4>
@@ -87,12 +101,12 @@ const Gallery = ({ match: { params: { gallerySlug } } }) => {
               </div>
             </div>
             {index > 0 && (
-              <img src='/previous.png' alt="Previous" className="previous" onClick={() => setIndex(index - 1)} />
+              <img src='/previous.png' alt="Previous" id="previous" className="previous" onClick={() => setIndex(index - 1)} />
             )}
             {index < gallery.photos.length - 1 && (
-              <img src='/next.png' alt="Next" className="next" onClick={() => setIndex(index + 1)} />
+              <img src='/next.png' alt="Next" id="next" className="next" onClick={() => setIndex(index + 1)} />
             )}
-            <img src='/exit.png' alt="Close" className="exit" onClick={() => setModalOpen(false)} />
+            <img src='/exit.png' alt="Close" id="exit" className="exit" onClick={closeModal} />
           </Modal>
         </>
       )}
