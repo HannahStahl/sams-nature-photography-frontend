20a16e3305394eaf4a0a9fcec87b22dc9b0df296
Hannah Stahl <hls62@cornell.edu>
2021-04-10 11:51:39 -0500

    Collapse photo details into info icon, and enable full-size view on mobile

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,6 +6,7 @@ import EnlargedPhoto from './EnlargedPhoto';
 
 const Gallery = ({ match }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [photoDetailsVisible, setPhotoDetailsVisible] = useState(false);
   const [index, setIndex] = useState(0);
 
   let gallery;
@@ -25,14 +26,16 @@ const Gallery = ({ match }) => {
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, [handleKeyDown]);
 
+  useEffect(() => {
+    setPhotoDetailsVisible(false);
+  }, [index, modalOpen]);
+
   const { galleries } = useContext(Context);
   if (galleries.length === 0 || !match) return <></>;
 
   const { params: { gallerySlug } } = match;
   gallery = galleries.find(({ slug: { current } }) => current === gallerySlug);
   if (!gallery) return <Redirect to="/page-not-found" />;
-
-  const mobile = window.innerWidth < 500 || window.innerHeight < 500;
 
   const selectedPhoto = gallery.photos[index];
   const { title, description, image: { asset: { url } } } = selectedPhoto;
@@ -70,33 +73,29 @@ const Gallery = ({ match }) => {
           </div>
         ))}
       </div>
-
-      {!mobile && (
-        <>
-          <Modal
-            centered
-            animation={false}
-            dialogAs={(props) => <EnlargedPhoto style={{ width, height }} {...props} />}
-            show={modalOpen}
-            onHide={() => setModalOpen(false)}
-          >
-            <div className="enlarged-photo-with-details">
-              <img className="enlarged-photo" src={url} alt={title} />
-              <div className="photo-details">
-                <h4>{title}</h4>
-                {description && <p>{description}</p>}
-              </div>
-            </div>
-            {index > 0 && (
-              <img src='/previous.png' alt="Previous" className="previous" onClick={() => setIndex(index - 1)} />
-            )}
-            {index < gallery.photos.length - 1 && (
-              <img src='/next.png' alt="Next" className="next" onClick={() => setIndex(index + 1)} />
-            )}
-            <img src='/exit.png' alt="Close" className="exit" onClick={() => setModalOpen(false)} />
-          </Modal>
-        </>
-      )}
+      <Modal
+        centered
+        animation={false}
+        dialogAs={(props) => <EnlargedPhoto style={{ width, height }} {...props} />}
+        show={modalOpen}
+        onHide={() => setModalOpen(false)}
+      >
+        <div className="enlarged-photo-with-details">
+          <img className="enlarged-photo" src={url} alt={title} />
+          <div className={`photo-details${photoDetailsVisible ? ' visible' : ''}`}>
+            <h4>{title}</h4>
+            {description && <p>{description}</p>}
+          </div>
+          <i className="photo-details-icon fas fa-info-circle" onClick={() => setPhotoDetailsVisible(!photoDetailsVisible)} />
+        </div>
+        {index > 0 && (
+          <img src='/previous.png' alt="Previous" className="previous" onClick={() => setIndex(index - 1)} />
+        )}
+        {index < gallery.photos.length - 1 && (
+          <img src='/next.png' alt="Next" className="next" onClick={() => setIndex(index + 1)} />
+        )}
+        <img src='/exit.png' alt="Close" className="exit" onClick={() => setModalOpen(false)} />
+      </Modal>
     </div>
   );
 };
